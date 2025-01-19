1fd0fc10d6c7772fbdf3f8a51cb7c69a67b69b03
noni <nsoni80000005@@gmaill.com>
2025-01-19 15:02:28 +0530

    edited

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,7 +3,7 @@ import { Routes, Route } from "react-router-dom";
 import Start from "./pages/Start";
 import UserLogin from "./pages/UserLogin";
 import UserSignup from "./pages/UserSignup";
-import CaptainLogin from "./pages/CaptainLogin";
+import CaptainLogin from "./pages/Captainlogin";
 import CaptainSignup from "./pages/CaptainSignup";
 import Home from "./pages/Home";
 import CaptainHome from "./pages/CaptainHome";
